5e7a5d2272e77c639a8a8c6c872ff05ec6ee7b17
Gerlenn <gleb_kozlov1998@mail.ru>
2022-10-02 21:53:22 +0300

    Created class MainRun(lesson 5)

---
+++ app/src/main/java/com/example/homework4/MainRun.java
@@ -0,0 +1,29 @@
+package com.example.homework4;
+
+public class MainRun {
+    public static void main(String[] args) {
+        Pigeon pigeon = new Pigeon();
+        Crow crow = new Crow();
+        pigeon.fly();
+        crow.fly();
+    }
+}
+
+class Pigeon extends Bird{
+    @Override
+    void fly() {
+        System.out.println("The pigeon is flying");
+    }
+}
+
+class Crow extends Bird{
+    @Override
+    void fly() {
+        System.out.println("The crow is flying");
+    }
+}
+
+abstract class Bird {
+    abstract void fly();
+}
+
