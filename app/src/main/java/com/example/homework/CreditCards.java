c274e1649ebd6d70e17aba8d3a1bb2c8d1b90393
Gerlenn <gleb_kozlov1998@mail.ru>
2022-10-04 16:52:06 +0300

    created class CreditCard(Task_1_HW6)

---
+++ app/src/main/java/com/example/homework/CreditCards.java
@@ -0,0 +1,44 @@
+package com.example.homework;
+
+public class CreditCards {
+
+    int accountNumber;// номер счёта
+    int currentAmount;// сумма на счёте
+    int generalAmount;// итоговая сумма на счёте
+
+    void creditingToTheAccount(int accrualAmount){// начисление на счёт
+        generalAmount = currentAmount + accrualAmount;
+    }
+
+    void withdrawFromTheAccount(int withdrawalAmount){// снятие со счёта
+        if (withdrawalAmount > currentAmount)
+            System.out.println("Недостаточно средств на счёте "+ accountNumber +"!" + "\nДля снятия доступно " + currentAmount + "\n");
+            generalAmount = currentAmount;
+        if (withdrawalAmount <= currentAmount)
+            generalAmount = currentAmount - withdrawalAmount;
+
+    }
+
+    void accountInformation(){
+        System.out.println("Номер Вашего счёта: " + accountNumber + "\n" + "Сумма на счёте: " + generalAmount + "\n");
+    }
+
+    CreditCards(int accountNumber, int currentAmount){
+        this.accountNumber = accountNumber;
+        this.currentAmount = currentAmount;
+    }
+
+    public static void main(String[] args) {
+        CreditCards cardOne = new CreditCards(1111, 10);
+        CreditCards cardTwo = new CreditCards(2222, 20);
+        CreditCards cardThree = new CreditCards(3333, 30);
+
+        cardOne.creditingToTheAccount(100);
+        cardTwo.creditingToTheAccount(500);
+        cardThree.withdrawFromTheAccount(31);
+
+        cardOne.accountInformation();
+        cardTwo.accountInformation();
+        cardThree.accountInformation();
+    }
+}
