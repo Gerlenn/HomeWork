5ee05177bbec2e63ab8cf32ad7e941d1b7a1945c
Gerlenn <gleb_kozlov1998@mail.ru>
2022-10-09 16:05:32 +0300

    Fixed class SeasonsOfTheYear(HW7)

--- app/src/main/java/com/example/homework/SeasonsOfTheYear.java
+++ app/src/main/java/com/example/homework/SeasonsOfTheYear.java
@@ -23,9 +23,13 @@ public class SeasonsOfTheYear {
     }
 
     public static void main(String[] args) {
+        SeasonsOfTheYear seasonsOfTheYear = new SeasonsOfTheYear();
+        seasonsOfTheYear.choiceSeason(Season.SPRING);
+
         Season season = Season.SPRING;
         String favoriteSeason = "Spring is the time of year when snowdrifts thaw and everything turns green little by little.\nFirst flowers come out and trees are in bloom. May is the warmest spring month. In May weather is usually very nice.\nWarm wind blows and the sun beams.\n";
-        System.out.println("My favorite season - " + Season.SPRING + "\n" + favoriteSeason);
+        System.out.println("My favorite season - " + season + "\n" + favoriteSeason);
+
 
         for (Season allSeasons : Season.values()) {
             System.out.println("Season: " + allSeasons + ". Average temperature of the season: " + allSeasons.temperature);
@@ -34,16 +38,16 @@ public class SeasonsOfTheYear {
     void choiceSeason(Season nameSeason) {
         switch (nameSeason) {
             case SUMMER:
-                System.out.println("I love summer");
+                System.out.println("I love summer\n");
                 break;
             case AUTUMN:
-                System.out.println("I love autumn");
+                System.out.println("I love autumn\n");
                 break;
             case WINTER:
-                System.out.println("I love winter");
+                System.out.println("I love winter\n");
                 break;
             case SPRING:
-                System.out.println("I love spring");
+                System.out.println("I love spring\n");
                 break;
         }
     }
