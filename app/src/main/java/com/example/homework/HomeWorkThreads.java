ea1638dc910f6d7e751cf6b0332fe72c68a93c22
Gerlenn <gleb_kozlov1998@mail.ru>
2022-10-30 13:44:32 +0300

    homework13

---
+++ app/src/main/java/com/example/homework/HomeWorkThreads.java
@@ -0,0 +1,55 @@
+package com.example.homework;
+
+import static java.lang.Thread.sleep;
+
+public class HomeWorkThreads {
+
+    public static void main(String[] args) {
+        //creating and running two threads(Thread and Runnable) - Task 1
+        ThreadClass threadClass = new ThreadClass();
+        threadClass.start();
+
+        ThreadClass1 threadClass1 = new ThreadClass1();
+        Thread thread1 = new Thread(threadClass1);
+        thread1.start();
+
+        //creating and running two threads by anonymous classes - Task 2
+        Thread thread = new Thread() {
+            @Override
+            public void run() {
+                System.out.println("Running (Thread) in anonymous class");
+            }
+        };
+        thread.start();
+
+        Runnable runnable = new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("Running (Runnable) in anonymous class");
+            }
+        };
+        //Passing a Runnable thread to another thread for execution and starting it - Task 3
+        Thread thread2 = new Thread(runnable);
+        thread2.start();
+    }
+}
+
+class ThreadClass extends Thread {
+
+    @Override
+    public void run() {
+        System.out.println("First Thread(Thread)");
+    }
+}
+
+class ThreadClass1 implements Runnable {
+
+    @Override
+    public void run() {
+        try {
+            sleep(5000);
+            Thread thread = Thread.currentThread();
+            System.out.println("I run this code in: " + thread.getName());
+        } catch (Exception e) {}
+    }
+}
